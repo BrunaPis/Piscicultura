3373a12bfe91aa3154fcdf8175ff88a0f2bb7984
Bruna <pinheirob323@gmail.com>
2023-10-04 13:47:22 -0300

    migração do banco

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Piscicultores, Viveiros, Peixes, Racao
 
+# Register your models here.
+admin.site.register(Piscicultores)
+admin.site.register(Viveiros)
+admin.site.register(Peixes)
+admin.site.register(Racao)
 # Register your models here.
